1dda22623a06c9c4a19b8c9a07f921d669e31f16
devansh-sharma-coditas <devanshsharmads9935@gmail.com>
2022-08-20 04:17:19 +0530

    Student System Completed !!

--- src/MenuDrivenTest/Student.java
+++ src/MenuDrivenTest/Student.java
@@ -24,15 +24,51 @@ public class Student {
                 ", contact='" + contact ;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
     public int getId() {
         return id;
     }
 
-    public static String toString(Student[] array, int id ) {
-        return  "id=" + id +
-                ", firstName='" + array[id].firstName + '\'' +
-                ", lastname='" + array[id].lastname + '\'' +
-                ", city='" + array[id].city + '\'' +
-                ", contact='" + array[id].contact ;
-    }
+//    public static String toString(Student[] array, int id ) {
+//        return  "id=" + id +
+//                ", firstName='" + array[id].firstName + '\'' +
+//                ", lastname='" + array[id].lastname + '\'' +
+//                ", city='" + array[id].city + '\'' +
+//                ", contact='" + array[id].contact ;
+//    }
 }
--- src/MenuDrivenTest/StudentOperations.java
+++ src/MenuDrivenTest/StudentOperations.java
@@ -1,11 +1,10 @@
 package MenuDrivenTest;
 
+import java.awt.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class StudentOperations {
-
-
     int id;
     String firstName;
     String lastname ;
@@ -23,21 +22,28 @@ public class StudentOperations {
         city = scanner.nextLine();
         System.out.println("Enter contact : ");
         contact = scanner.nextLine();
-        array[count++] =new Student(id,firstName,lastname,city,contact);
-        System.out.println("Record "+ count + " Added succesfully !!");
+        array[count] =new Student(id,firstName,lastname,city,contact);
+        System.out.println("Record "+ (count+1) + " Added succesfully !!");
+        System.out.println("----------------------------------------------------------------------------");
     }
     public void fetchAllData(Student[] array ){
-        System.out.println("details of Student !!");
-        System.out.println(Arrays.toString(array));
+        System.out.println("Records of Students !!");
+        for(int i =0; i<array.length ; i++){
+            if(array[i] != null)
+                System.out.println(array[i].toString());
+        }
+        System.out.println("-----------------------------------------------------------------------------");
     }
-    public static void fetchData(Student[] array , int id  ){
+    public static void fetchData(Student[] array , int updateId  ){
+        System.out.println("Your last updated Data !!");
+        System.out.println(array[getIndex(array,updateId)].toString());
 
-        System.out.println(Student.toString(array,id));
     }
     public static int getIndex(Student[] array ,int id){
         int index = 0;
         for(int i=0 ;i <array.length ;i++){
-                if(id == array[i].id) index = i;
+                if(array[i] !=null)
+                    if(id == array[i].id ) index = i;
         }
         return index;
     }
@@ -45,22 +51,72 @@ public class StudentOperations {
         System.out.println("Enter your ID :");
         int updateID= scanner.nextInt();
         fetchData(array , updateID);
+        // catching that index where we want to perform updation
         int index = getIndex(array, updateID);
-        System.out.println("Which field you want to edit !!\n Enter 1 for id \nEnter 2 firstName \nEnter 3 for " +
-                "lastName \n Enter 4 for city \n Enter 5 for contact ");
-        int editChoice = scanner.nextInt();
 
-        switch(editChoice){
-            case 1 :
-                array[index].
+        int loopBreak=1;
+        // this loop will give chance to user to change data fields multiple times!
+        while(loopBreak !=0) {
+            System.out.println("Which field you want to edit !!\nEnter 1 for id \nEnter 2 firstName \n" +
+                    "Enter 3 for lastName \nEnter 4 for city \nEnter 5 for contact \nEnter 0 for exit !! ");
+            int editChoice = scanner.nextInt();
 
+            switch (editChoice) {
+                case 0:
+                    System.out.println("Now you are out from updateMenu !!");
+                    loopBreak= 0;
+                    break;
+                case 1 :
+                    System.out.println("Enter ID : ");
+                    //setting updated data to that index
+                    array[index].id = scanner.nextInt();
+                    System.out.println("Your ID updated succesfully !!");
+                    //showing data after updation
+                    fetchData(array , updateID);
+                    break;
+                case 2 :
+                    System.out.println("Enter FirstName : ");
+                    scanner.nextLine();
+                    array[index].firstName = scanner.nextLine();
+                    System.out.println("Your FirstName updated succesfully !!");
+                    fetchData(array , updateID);
+                    break;
+                case 3 :
+                    System.out.println("Enter LastName : ");
+                    scanner.nextLine();
+                    array[index].lastname = scanner.nextLine();
+                    System.out.println("Your LastName updated succesfully !!");
+                    fetchData(array , updateID);
+                    break;
+                case 4 :
+                    System.out.println("Enter city : ");
+                    scanner.nextLine();
+                    array[index].city = scanner.nextLine();
+                    System.out.println("Your City updated succesfully !!");
+                    fetchData(array , updateID);
+                    break;
+                case 5 :
+                    System.out.println("Enter contact : ");
+                    scanner.nextLine();
+                    array[index].contact = scanner.nextLine();
+                    System.out.println("Your City updated succesfully !!");
+                    fetchData(array , updateID);
+                    break;
+                default:
+                    System.out.println("Something Went Wrong !!");
+                    break;
+            }
         }
-
-
+        System.out.println("Data updated Succesfully \n ----------------------------------------------------------");
     }
     public void removeData(Student[] array ,Scanner scanner ){
         System.out.println("Enter id which you want to delete !!");
         int removeID = scanner.nextInt();
         array[getIndex(array,removeID)] = null;
+        System.out.println("Data Removed Successfully !!");
+        System.out.println("------------------------------------------------------------------------------");
     }
 }
+/*
+
+ */
